2fae667ecc734084d80e87907e39884cc9a045fa
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-09 08:38:18 -0300

    CriacaoDeNovaRota

---
+++ controllers/UserController.js
@@ -0,0 +1,10 @@
+class UserController{
+    async index(req, res){}
+
+    async create(req, res){
+        console.log(req.body);
+        res.send("Pegando corpo da requisição");
+    }
+}
+
+module.exports = new UserController();
