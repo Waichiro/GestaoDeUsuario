b84fc841f24e790d90f1d6ee660a1393bf9b96f0
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-09 09:34:46 -0300

    Adicionando validacoes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,25 @@ class UserController{
     async index(req, res){}
 
     async create(req, res){
-        console.log(req.body);
+        
+        var {email, name, password, repassword} = req.body;
+
+        if(email == undefined){
+            res.status(400);
+            res.json({err: "email invalido"});
+        }
+
+        if(name == undefined || name.length < 3){
+            res.status(400);
+            res.json({err: "Nome invalido"});
+        }
+
+        if(password == undefined || password != repassword || repassword != password){
+            res.status(400);
+            res.json({err: "Senha invalido"});
+        }
+
+        res.status(200);
         res.send("Pegando corpo da requisição");
     }
 }
