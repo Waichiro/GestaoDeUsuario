777cfe71ab8039ca4117fa3520828ff5e618054f
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-09 14:54:55 -0300

    Autenticaçao

--- routes/routes.js
+++ routes/routes.js
@@ -3,13 +3,14 @@ var app = express();
 var router = express.Router();
 var HomeController = require("../controllers/HomeController");
 var UserController = require("../controllers/UserController");
+var AdminAuth = require("../middleware/AdminAuth");
 
 router.get('/', HomeController.index);
-router.post('/user', UserController.create);
-router.get("/user", UserController.index);
-router.get("/user/:id", UserController.findUsers);
+router.post('/user',AdminAuth, UserController.create);
+router.get("/user",AdminAuth, UserController.index);
+router.get("/user/:id",AdminAuth, UserController.findUsers);
 router.put("/user", UserController.edit);
-router.delete("/user/:id", UserController.remove);
+router.delete("/user/:id",AdminAuth, UserController.remove);
 router.post("/recoverpassword", UserController.recoverPassword);
 router.post("/changepassword", UserController.changePassword);
 router.post("/login", UserController.login);
