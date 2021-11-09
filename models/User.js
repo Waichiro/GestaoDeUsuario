74e09b333feda12a55501af31e08c960dce4d61b
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-09 11:35:10 -0300

    AddDeletarUsuario

--- models/User.js
+++ models/User.js
@@ -119,6 +119,22 @@ class User{
         }
     }
 
+    async delete(id){
+        var user = await this.findById(id);
+
+        if(user != undefined){
+            try{
+                await knex.delete().where({id: id}).table("users");
+                return {status: true};
+            }catch(err){
+                return {status: false, err: err}
+            }
+            
+        }else{
+            return {status: false, err: "O usuario não esxite entao não pode ser deletado"}
+        }
+    }
+
 }
 
 module.exports = new User;
