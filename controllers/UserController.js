023b1e2d8de8cf285cc52db3cc29f7e52c920c81
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-11-09 10:16:19 -0300

    AddhashSegurancaNaSenha

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+var User = require("../models/User");
+
 class UserController{
     async index(req, res){}
 
@@ -8,17 +10,32 @@ class UserController{
         if(email == undefined){
             res.status(400);
             res.json({err: "email invalido"});
+            return;
         }
 
         if(name == undefined || name.length < 3){
             res.status(400);
             res.json({err: "Nome invalido"});
+            return;
         }
 
         if(password == undefined || password != repassword || repassword != password){
             res.status(400);
             res.json({err: "Senha invalido"});
+            return;
         }
+
+        var emailExists = await User.findEmail(email);
+
+        if(emailExists){
+            res.status(406);
+            res.json({err: "ja existe cadastro com esse email"})
+            return;
+        }
+
+        
+
+        await User.new(email, password, name);
 
         res.status(200);
         res.send("Pegando corpo da requisição");
